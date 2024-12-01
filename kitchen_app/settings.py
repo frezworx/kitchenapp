87a0ec63b5cb4ff4f1dbe76e5c8beedd621dab9f
Frezworx <frezworx@gmail.com>
2024-12-01 12:19:40 +0200

    Added  data files for fill db

--- kitchen_app/settings.py
+++ kitchen_app/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = "kitchen_app.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": ["templates", ],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
--- kitchen_app/urls.py
+++ kitchen_app/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("core_app"))
+    path("", include("core_app.urls")),
 ]
